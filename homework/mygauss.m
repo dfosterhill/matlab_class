291a173ba3917cdb930f325e6f81d0ea5daaa74d
annarhughes1421 <annarhughes1421@gmail.com>
2021-10-12 14:51:08 -0700

    Added to test in mygauss.m

--- homework/mygauss.m
+++ homework/mygauss.m
@@ -25,3 +25,9 @@ ylabel('Sum');
 %% other to test with not 3 columns
 m_4 = [3,4;5,6;1,2]
 multGauss(m_4,x)
+
+%% other tests 
+x = 0:0.1:20; 
+m_6 = [1, 1, 0.5;2, 2, 1;3, 3, 2]; %other to test
+figure(3)
+plot(x,gaussmf(x, [1,0.5]),x, gaussmf(x, [2,1]), x, gaussmf(x, [3,2]))
