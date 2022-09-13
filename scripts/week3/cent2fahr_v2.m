b0ec1f6dd27b6153aeaed838fe4f3263e37e2f13
dfosterhill <dfh@oregonstate.edu>
2022-09-13 09:03:12 -0700

    refresh repo for fall 2022

--- scripts/week3/cent2fahr_v2.m
+++
@@ -1,9 +0,0 @@
-function TF=cent2fahr_v2(TC)
-%This function will convert temperature in centigrade to fahrenheit
-%Created by NAME on DATE
-%-
-%TC - (input) temperature in centigrade
-%TF - (output) temperature in Fahrenheit
-
-TF=TC*9/5+32;
-TC=NaN
--- scripts/week3/cent2fahr_v3.m
+++
@@ -1,8 +0,0 @@
-function TF=cent2fahr_v3(TC,scale,offset)
-%This function will convert temperature in centigrade to fahrenheit
-%Created by NAME on DATE
-%-
-%TC - (input) temperature in centigrade
-%TF - (output) temperature in Fahrenheit
-
-TF=TC*scale+offset;
