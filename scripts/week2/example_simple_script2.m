386ecb8d30dc3b82bb53bff15598334ae7ba2157
dfosterhill <dfh@oregonstate.edu>
2023-09-05 10:25:56 -0700

    update

--- scripts/week2/example_simple_script2.m
+++ scripts/week2/example_simple_script2.m
@@ -13,13 +13,20 @@
 % $$y(t)=V\sin(\theta)\; t -\frac{1}{2}gt^{2}$$
 % 
 % where g is the gravitational acceleration. Therefore, the trajectory is controlled 
-% by the initial speed and the launch angle.
+% by the initial speed and the launch angle. If you are curious about how I inserted 
+% equations, please to go the _insert_ tab and click on the _equation_ option. 
+% You can use an equation editor (much like found in MS Word), or you can choose 
+% to insert an equation using <https://www.overleaf.com/learn/latex/Mathematical_expressions 
+% LaTeX> commands. Note also that you can include hyperlinks (see previous sentence).
 % Interactive plot
-% Below, select V (m/s) and $\theta$ (deg)
+% Below, select V (m/s) and $\theta$ (deg). Note: it's pretty cool that you 
+% can insert interactive <https://www.mathworks.com/help/matlab/matlab_prog/add-interactive-controls-to-a-live-script.html 
+% controls> like the sliders below. Again, see the _insert_ menu and look for 
+% the _control_ drop-down box. 
 
 %define the parameters
-V = 6 
-theta = 16
+V = 18.6 
+theta = 76
 g = 9.81;    %gravity in SI units
 %solve for final time
 tf=2*V*sind(theta)/g;
