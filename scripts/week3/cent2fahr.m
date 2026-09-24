5f1b0cce26e604b7bad24082de33e16f5dc7fbd9
dfosterhill <dfh@oregonstate.edu>
2026-09-24 09:44:18 -0700

    prep for FA26

--- scripts/week3/cent2fahr.m
+++
@@ -1,9 +0,0 @@
-function TF=cent2fahr(TC)
-
-%This function will convert temperature in centigrade to fahrenheit
-%Created by D.F. Hill - Sep 2023
-%
-%TC - (input) temperature in centigrade
-%TF - (output) temperature in Fahrenheit
-
-TF=TC*9/5+32;
--- scripts/week3/cent2fahr_v3.m
+++
@@ -1,9 +0,0 @@
-function TF=cent2fahr_v3(TC,scale,offset)
-
-%This function will convert temperature in centigrade to fahrenheit
-%Created by D.F. Hill - Sep, 2023
-%-
-%TC - (input) temperature in centigrade
-%TF - (output) temperature in Fahrenheit
-
-TF=TC*scale+offset;
